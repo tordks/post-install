19694965429afde2745c83ac972cd4e8bda3d2e8
tordks <tordks@hotmail.com>
2015-10-07 20:33:03 +0200

    Removed redundant lines

--- post-install.sh
+++ post-install.sh
@@ -86,7 +86,6 @@ vlc \
 skype \
 wine \
 indicator-stickynotes
-#|| echo "Installation failed" && exit
 
 #tor-browser \
 
@@ -176,7 +175,6 @@ timeshift \
 curl \
 cifs-utils \
 openconnect 
-#|| echo "Installation failed" && exit
 
 #when-changed
 sudo pip install https://github.com/joh/when-changed/archive/master.zip 
@@ -267,10 +265,7 @@ cmake \
 texlive \
 texlive-latex-extra \
 latex-xcolor \
-latexmk \
-|| echo "Installation failed" && exit
-
-
+latexmk
 }
 
 
@@ -300,7 +295,7 @@ icontool \
 inkscape \
 imagemagick \
 blender \
-'gnome-do' || echo "Installation failed" && exit
+'gnome-do' 
 }
 
 
